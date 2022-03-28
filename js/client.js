279391d44f03e2c9777a328446c9058715855194
Ludivine Langry <97959312+Lounacoeur@users.noreply.github.com>
2022-03-28 15:11:36 +0200

    Add files via upload

---
+++ js/client.js
@@ -0,0 +1,45 @@
+var socket = io();
+
+//Variable qui va définir un destinataire, par défaut le salon général
+var id_salon ='salon';
+//Tableau qui va contenir l'ensemble des messages envoyés (semi-persistance)
+var lesMessages = [];
+
+// Selection pseudo
+socket.emit('set-pseudo',prompt("Pseudo ?"));
+
+// Variables pour récupérer les éléments HTML
+var messages = document.getElementById('messages');
+var form = document.getElementById('form');
+var input = document.getElementById('input');
+
+// Écouteur et envoi du message du formulaire
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit('emission_message', input.value);
+    input.value = '';
+  }
+});
+
+// Réception et affichage du message
+socket.on('reception_message', (contenu) => {
+  var item = document.createElement('li');
+  item.textContent = contenu.pseudo+" : "+contenu.msg;
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+  
+  var lesMessages = lesMessages + 'reception_message';
+});
+
+// Affichage des messages en fonction du choix de l'utilisateur :
+// Soit les messages du salon général ;
+// Soit les messages d'une conversation privée avec un autre utilisateur
+function salon(id){
+  /* A compléter */
+}
+
+// Vérifie les messages non-lus, puis affiche un badge de notification incrémenté à côté de l'utilisateur en question
+function check_unread(){
+  /* A compléter */
+}
---
+++ js/server.js
@@ -0,0 +1,75 @@
+// Configuration du serveur, et des modules
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+var path = require("path");
+let PORT = 8080;
+
+// Port d'écoute
+server.listen(PORT, () => {
+  console.log('Serveur démarré sur le port :'+PORT);
+});
+
+// Route page d'accueil
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
+
+// Route script partie client
+app.get('/client',(req, res) =>{
+  res.sendFile(__dirname + '/client.js');
+});
+
+// Route script CSS
+app.get('/style.css',(req,res)=>{
+  res.sendFile(path.join(__dirname,'..','/css/style.css'));
+});
+
+// Recensement de l'utilisateur
+function user() {
+  io.fetchSockets().then((room)=> {
+    var user = [];
+      room.forEach(element => {
+        user.push(element.nickname);
+        console.log(user)
+      });
+      io.emit('reception_pseudo', user);
+  });
+}
+
+//Récupération de la liste des utilisateurs (Sockets) connectés
+io.fetchSockets().then((room)=> {
+  var utilisateurs=[];
+  room.forEach((item) => {
+    utilisateurs.push({
+      id_client : item.id,
+      pseudo_client : item.pseudo,
+    });
+  });
+  io.emit('reception_utilisateur',utilisateurs);
+});
+
+// Lancement du gestionnaire d'événements, qui va gérer notre Socket
+io.on('connection',(socket)=>{
+  // Socket de saisie du pseudo
+  socket.on('set-pseudo',(pseudo)=>{
+    console.log(pseudo + " vient de se connecter à "+new Date());
+    socket.nickname = pseudo;
+  });
+
+  // Socket pour l'émission/reception des messages
+  socket.on('emission_message',(message)=>{
+    io.emit("reception_message",{
+      pseudo : socket.nickname,
+      msg : message
+    });
+  });
+  
+  // Socket log serveur pour la déconnexion
+  socket.on('disconnect',()=>{
+    console.log(socket.nickname+ " s'est déconnecté");
+  });
+});
